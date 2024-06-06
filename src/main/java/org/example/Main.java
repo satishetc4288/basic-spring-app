b566ce1fcfaea9276f42f9e10f744f76072dd67a
satish rajput <satishetc4288@gmail.com>
2024-06-06 21:40:21 +0530

    all well

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,27 @@
 package org.example;
 
+import java.sql.*;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Arrays.stream(args).forEach(System.out::println);
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn =
+            DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root", "root");
+        Statement ps = conn.createStatement();
+        Arrays.stream(args).forEach( arg -> {
+            ResultSet rs = null;
+            try {
+                rs = ps.executeQuery(arg + ";");
+                while(rs.next()){
+                    System.out.println(rs.getString(1) + " " +
+                            rs.getString(2) + " " +
+                            rs.getString(3) );
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
